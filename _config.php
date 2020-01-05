0ec49eb64fde4dac49e17b71b316c9dea0c43503
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-01-05 23:16:56 +0000

    Initial checkin

---
+++ _config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * SilverStripe Dark Sky configuration file.
+ *
+ * PHP version >=5.6.0
+ *
+ * For full copyright and license information, please view the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * @package Suilven\DarkSky
+ * @author Gordon Anderson <gordon.b.anderson@gmail.com>
+ * @copyright 2020 n/a
+ * @license https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @link https://github.com/suilven/silverstripe-darksky
+ */
